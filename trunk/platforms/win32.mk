81f3a73ac431464f0e75d9b46e3d67345443cff4
nishi <nishi@0f02c867-ac3d-714e-8a88-971ba1f6efcf>
2024-04-23 02:06:43 +0000

    some
    git-svn-id: https://sw.nishi.boats/svn/nishi-serenade@3 0f02c867-ac3d-714e-8a88-971ba1f6efcf

---
+++ trunk/platforms/win32.mk
@@ -0,0 +1,5 @@
+# $Id$
+# Copy me to trunk/platform.mk
+
+CC = i686-w64-mingw32-gcc
+SUFFIX = .exe
---
+++ trunk/platforms/win64.mk
@@ -0,0 +1,5 @@
+# $Id$
+# Copy me to trunk/platform.mk
+
+CC = x86_64-w64-mingw32-gcc
+SUFFIX = .exe
