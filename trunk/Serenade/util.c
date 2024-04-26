65fa0d86265232bc8e9c9c7d8facd8332df3eeea
nishi <nishi@0f02c867-ac3d-714e-8a88-971ba1f6efcf>
2024-04-26 05:21:26 +0000

    worky
    git-svn-id: https://sw.nishi.boats/svn/nishi-serenade@46 0f02c867-ac3d-714e-8a88-971ba1f6efcf

--- trunk/Serenade/util.c
+++ trunk/Serenade/util.c
@@ -57,7 +57,10 @@ void sn_print_to(FILE* f, struct sn_generic* gen) {
 	} else if(gen->type == SN_TYPE_VOID) {
 		fprintf(f, "<void>");
 	} else if(gen->type == SN_TYPE_FUNCTION) {
-		fprintf(f, "<function %s>", gen->name == NULL ? "(anonymous)" : gen->name);
+		fprintf(f, "<function %s", gen->name == NULL ? "(anonymous)" : gen->name);
+		if(gen->name == NULL){
+			fprintf(f, ":%x>", gen->handler);
+		}
 	} else if(gen->type == SN_TYPE_PTR) {
 		fprintf(f, "<pointer %x>", gen->ptr);
 	}
