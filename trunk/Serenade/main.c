d6c7a3ebe634e2185f183f8bac7967bd9a1a9dd5
nishi <nishi@0f02c867-ac3d-714e-8a88-971ba1f6efcf>
2024-04-26 23:26:25 +0000

    better s
    git-svn-id: https://sw.nishi.boats/svn/nishi-serenade@53 0f02c867-ac3d-714e-8a88-971ba1f6efcf

--- trunk/Serenade/main.c
+++ trunk/Serenade/main.c
@@ -85,11 +85,11 @@ int main(int argc, char** argv) {
 	if(!loaded) {
 		is_repl = true;
 		printf("  ---+-------+---    oooo                                            8       \n");
-		printf("  ---+-------+---   8                                                8       \n");
+		printf("  ---+-------+---   8    8                                           8       \n");
 		printf("     |       |      8       oooo  8 oooo   oooo  8 oooo   oooo   oooo8  oooo \n");
 		printf("     |       |       oooo  8    8 8o    o 8    8 8o    8      8 8    8 8    8\n");
 		printf("     |       |           8 8oooo8 8       8oooo8 8     8  oooo8 8    8 8oooo8\n");
-		printf("     |       |           8 8      8       8      8     8 8    8 8    8 8     \n");
+		printf("     |       |      9    8 8      8       8      8     8 8    8 8    8 8     \n");
 		printf("     |       |       oooo   oooo  8        oooo  8     8  oooo8  oooo8  oooo \n");
 		printf("\n");
 		printf("Welcome to Serenade LISP %s <http://nishi.boats/serenade>\n", SERENADE_VERSION);
